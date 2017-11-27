d85759a27021efb825ad64cbd92bc6095d7f0507
slin12 <s.lin12@gmail.com>
2017-11-27 11:55:02 -0500

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static#home'
+  get '/projects/1', to: 'static#one'
+end
