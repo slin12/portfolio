c0d6a402885275f33423af8539a2e63b5533b710
Shirley <s.lin12@gmail.com>
2017-12-04 09:20:54 -0500

    change arrow functions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,19 +13,16 @@
 //= require turbolinks
 //= require_tree .
 
-document.addEventListener('DOMContentLoaded', event => {
-  const canvasExperiments = [`<div class="cp_embed_wrapper"><iframe id="cp_embed_QOJXyG" src="//codepen.io/shirshirlove/embed/QOJXyG?height=400&amp;theme-id=dark&amp;slug-hash=QOJXyG&amp;default-tab=result&amp;user=shirshirlove&amp;embed-version=2&amp;pen-title=Snow%20Days" scrolling="no" frameborder="0" height="400" allowtransparency="true" allowfullscreen="true" name="CodePen Embed" title="Snow Days" class="cp_embed_iframe " style="width: 100%; overflow: hidden;"></iframe></div>
-<script async="" src="https://production-assets.codepen.io/assets/embed/ei.js"></script>`,
-`<div class="cp_embed_wrapper"><iframe id="cp_embed_ZaVMbq" src="//codepen.io/shirshirlove/embed/ZaVMbq?height=400&amp;theme-id=dark&amp;slug-hash=ZaVMbq&amp;default-tab=result&amp;user=shirshirlove&amp;embed-version=2&amp;pen-title=Snow%20Days" scrolling="no" frameborder="0" height="400" allowtransparency="true" allowfullscreen="true" name="CodePen Embed" title="Snow Days" class="cp_embed_iframe " style="width: 100%; overflow: hidden;"></iframe></div>
-<script async="" src="https://production-assets.codepen.io/assets/embed/ei.js"></script>`,
-`<div class="cp_embed_wrapper"><iframe id="cp_embed_Kybbbb" src="//codepen.io/shirshirlove/embed/Kybbbb?height=400&amp;theme-id=dark&amp;slug-hash=Kybbbb&amp;default-tab=result&amp;user=shirshirlove&amp;embed-version=2&amp;pen-title=Snow%20Days" scrolling="no" frameborder="0" height="400" allowtransparency="true" allowfullscreen="true" name="CodePen Embed" title="Snow Days" class="cp_embed_iframe " style="width: 100%; overflow: hidden;"></iframe></div>
-<script async="" src="https://production-assets.codepen.io/assets/embed/ei.js"></script>`]
+document.addEventListener('DOMContentLoaded', function(event) {
+  const canvasExperiments = ['<div class="cp_embed_wrapper"><iframe id="cp_embed_QOJXyG" src="//codepen.io/shirshirlove/embed/QOJXyG?height=400&amp;theme-id=dark&amp;slug-hash=QOJXyG&amp;default-tab=result&amp;user=shirshirlove&amp;embed-version=2&amp;pen-title=Snow%20Days" scrolling="no" frameborder="0" height="400" allowtransparency="true" allowfullscreen="true" name="CodePen Embed" title="Snow Days" class="cp_embed_iframe " style="width: 100%; overflow: hidden;"></iframe></div><script async="" src="https://production-assets.codepen.io/assets/embed/ei.js"></script>',
+'<div class="cp_embed_wrapper"><iframe id="cp_embed_ZaVMbq" src="//codepen.io/shirshirlove/embed/ZaVMbq?height=400&amp;theme-id=dark&amp;slug-hash=ZaVMbq&amp;default-tab=result&amp;user=shirshirlove&amp;embed-version=2&amp;pen-title=Snow%20Days" scrolling="no" frameborder="0" height="400" allowtransparency="true" allowfullscreen="true" name="CodePen Embed" title="Snow Days" class="cp_embed_iframe " style="width: 100%; overflow: hidden;"></iframe></div><script async="" src="https://production-assets.codepen.io/assets/embed/ei.js"></script>',
+'<div class="cp_embed_wrapper"><iframe id="cp_embed_Kybbbb" src="//codepen.io/shirshirlove/embed/Kybbbb?height=400&amp;theme-id=dark&amp;slug-hash=Kybbbb&amp;default-tab=result&amp;user=shirshirlove&amp;embed-version=2&amp;pen-title=Snow%20Days" scrolling="no" frameborder="0" height="400" allowtransparency="true" allowfullscreen="true" name="CodePen Embed" title="Snow Days" class="cp_embed_iframe " style="width: 100%; overflow: hidden;"></iframe></div><script async="" src="https://production-assets.codepen.io/assets/embed/ei.js"></script>']
 
   const leftArrow = document.getElementById('left-arrow');
   const rightArrow = document.getElementById('right-arrow');
   const demoDiv = document.querySelector('.demo');
 
-  leftArrow.addEventListener('click', event => {
+  leftArrow.addEventListener('click', function(event) {
     event.preventDefault();
     if (parseInt(demoDiv.id) > 0) {
       demoDiv.innerHTML = canvasExperiments[parseInt(demoDiv.id) - 1];
@@ -38,7 +35,7 @@ document.addEventListener('DOMContentLoaded', event => {
 
   })
 
-  rightArrow.addEventListener('click', event => {
+  rightArrow.addEventListener('click', function(event) {
     event.preventDefault();
     if (parseInt(demoDiv.id) < 2) {
       demoDiv.innerHTML = canvasExperiments[parseInt(demoDiv.id) + 1];
