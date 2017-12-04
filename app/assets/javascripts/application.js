8f83295b33acb57d4ae6e26cf3024efaa0bcc0eb
Shirley <s.lin12@gmail.com>
2017-12-04 08:59:45 -0500

    add javascript for carousel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
@@ -13,3 +12,42 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+document.addEventListener('DOMContentLoaded', event => {
+  const canvasExperiments = [`<div class="cp_embed_wrapper"><iframe id="cp_embed_QOJXyG" src="//codepen.io/shirshirlove/embed/QOJXyG?height=400&amp;theme-id=dark&amp;slug-hash=QOJXyG&amp;default-tab=result&amp;user=shirshirlove&amp;embed-version=2&amp;pen-title=Snow%20Days" scrolling="no" frameborder="0" height="400" allowtransparency="true" allowfullscreen="true" name="CodePen Embed" title="Snow Days" class="cp_embed_iframe " style="width: 100%; overflow: hidden;"></iframe></div>
+<script async="" src="https://production-assets.codepen.io/assets/embed/ei.js"></script>`,
+`<div class="cp_embed_wrapper"><iframe id="cp_embed_ZaVMbq" src="//codepen.io/shirshirlove/embed/ZaVMbq?height=400&amp;theme-id=dark&amp;slug-hash=ZaVMbq&amp;default-tab=result&amp;user=shirshirlove&amp;embed-version=2&amp;pen-title=Snow%20Days" scrolling="no" frameborder="0" height="400" allowtransparency="true" allowfullscreen="true" name="CodePen Embed" title="Snow Days" class="cp_embed_iframe " style="width: 100%; overflow: hidden;"></iframe></div>
+<script async="" src="https://production-assets.codepen.io/assets/embed/ei.js"></script>`,
+`<div class="cp_embed_wrapper"><iframe id="cp_embed_Kybbbb" src="//codepen.io/shirshirlove/embed/Kybbbb?height=400&amp;theme-id=dark&amp;slug-hash=Kybbbb&amp;default-tab=result&amp;user=shirshirlove&amp;embed-version=2&amp;pen-title=Snow%20Days" scrolling="no" frameborder="0" height="400" allowtransparency="true" allowfullscreen="true" name="CodePen Embed" title="Snow Days" class="cp_embed_iframe " style="width: 100%; overflow: hidden;"></iframe></div>
+<script async="" src="https://production-assets.codepen.io/assets/embed/ei.js"></script>`]
+
+  const leftArrow = document.getElementById('left-arrow');
+  const rightArrow = document.getElementById('right-arrow');
+  const demoDiv = document.querySelector('.demo');
+
+  leftArrow.addEventListener('click', event => {
+    event.preventDefault();
+    if (parseInt(demoDiv.id) > 0) {
+      demoDiv.innerHTML = canvasExperiments[parseInt(demoDiv.id) - 1];
+      demoDiv.id = parseInt(demoDiv.id) - 1;
+    }
+    else {
+      demoDiv.innerHTML = canvasExperiments[2];
+      demoDiv.id = 2;
+    }
+
+  })
+
+  rightArrow.addEventListener('click', event => {
+    event.preventDefault();
+    if (parseInt(demoDiv.id) < 2) {
+      demoDiv.innerHTML = canvasExperiments[parseInt(demoDiv.id) + 1];
+      demoDiv.id = parseInt(demoDiv.id) + 1;
+    }
+    else {
+      demoDiv.innerHTML = canvasExperiments[0];
+      demoDiv.id = 0;
+    }
+
+  })
+})
